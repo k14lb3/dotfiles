552dedb4ab980d467288de79f755616d8aa5aebe
k14lb3 <k14lb3@gmail.com>
2022-07-25 16:13:06 +0800

    [nvim] Change cmp mapping
    - Change cmp.mapping.scroll_docs(-4) mapping from <C-b> to <C-f> because tmux prefix is set to <C-b>
    - Remove triggering of cmp.complete() when pressing Tab if there are words before the cursor

--- neovim/.config/nvim/lua/plugins/config/cmp.lua
+++ neovim/.config/nvim/lua/plugins/config/cmp.lua
@@ -3,11 +3,6 @@ local config = function()
   local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
   local luasnip = require 'luasnip'
   local lspkind = require 'lspkind'
-  local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-  end
-
   cmp.setup {
     snippet = {
       expand = function(args)
@@ -28,7 +23,7 @@ local config = function()
       format = lspkind.cmp_format(),
     },
     mapping = cmp.mapping.preset.insert {
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
@@ -38,8 +33,6 @@ local config = function()
           cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
         else
           fallback()
         end
