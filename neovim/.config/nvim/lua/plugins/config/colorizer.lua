6c905cc4167b5a61fd6fae875511086a60fe8fd9
k14lb3 <k14lb3@gmail.com>
2022-09-24 23:39:22 +0800

    [nvim] Fix nvim-colorizer disregarding 'names = false' option norcalli/nvim-colorizer.lua#85

--- neovim/.config/nvim/lua/plugins/config/colorizer.lua
+++ neovim/.config/nvim/lua/plugins/config/colorizer.lua
@@ -1,24 +1,15 @@
 local config = function()
   local colorizer = require 'colorizer'
 
-  -- #f0f
-  -- #ff00ffff
-  -- Magenta
-  -- #ff00ff
-  -- rgb(255, 0, 255)
-  -- rgba(255, 0, 255, 0.75)
-  -- hsl(300, 100%, 50%)
-  -- hsla(300, 100%, 50%, 0.75)
-
   colorizer.setup({ '*' }, {
-    RGB = false,
-    RRGGBB = false,
+    RGB = true,
+    RRGGBB = true,
     names = false,
-    RRGGBBAA = false,
-    rgb_fn = false,
-    hsl_fn = false,
-    css = true,
-    css_fn = true,
+    RRGGBBAA = true,
+    rgb_fn = true,
+    hsl_fn = true,
+    css = false,
+    css_fn = false,
   })
 end
 
