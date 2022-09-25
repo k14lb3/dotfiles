914d4607ecc0e9b3a04d345280c8d6f915ef5af3
k14lb3 <k14lb3@gmail.com>
2022-09-25 11:15:27 +0800

    [nvim] Fix wrong implementation of the solution for the issue LOL 52a63ac0f05c5fcdaaaf9e730d43f8c96c22997b

--- neovim/.config/nvim/lua/plugins/config/lsp/null.lua
+++ neovim/.config/nvim/lua/plugins/config/lsp/null.lua
@@ -4,6 +4,32 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
+local eslint_config_file_formats = {
+  -- https://eslint.org/docs/latest/user-guide/configuring/configuration-files-new#configuration-file
+  'eslint.config.js',
+  -- https://eslint.org/docs/user-guide/configuring/configuration-files#configuration-file-formats
+  '.eslintrc',
+  '.eslintrc.js',
+  '.eslintrc.cjs',
+  '.eslintrc.yaml',
+  '.eslintrc.yml',
+  '.eslintrc.json',
+}
+
+local prettier_config_file_formats = {
+  -- https://prettier.io/docs/en/configuration.html
+  '.prettierrc',
+  '.prettierrc.json',
+  '.prettierrc.yml',
+  '.prettierrc.yaml',
+  '.prettierrc.json5',
+  '.prettierrc.js',
+  '.prettierrc.cjs',
+  '.prettier.config.js',
+  '.prettier.config.cjs',
+  '.prettierrc.toml',
+}
+
 null_ls.setup {
   sources = {
     formatting.stylua.with {
@@ -20,23 +46,23 @@ null_ls.setup {
     },
     code_actions.eslint_d.with {
       condition = function(utils)
-        return utils.root_has_file(diagnostics.eslint.filetypes)
+        return utils.root_has_file(eslint_config_file_formats)
       end,
     },
     diagnostics.eslint_d.with {
       condition = function(utils)
-        return utils.root_has_file(diagnostics.eslint.filetypes)
+        return utils.root_has_file(eslint_config_file_formats)
       end,
     },
     formatting.prettierd.with {
       condition = function(utils)
-        return utils.root_has_file(formatting.prettier.filetypes)
+        return utils.root_has_file(prettier_config_file_formats)
       end,
     },
     formatting.eslint_d.with {
       -- Only register eslint_d if prettierd is not running as a formatter
       condition = function(utils)
-        return utils.root_has_file(diagnostics.eslint.filetypes)
+        return utils.root_has_file(eslint_config_file_formats)
           and #null_ls_sources.get { name = 'prettierd', method = null_ls.methods.FORMATTING } == 0
       end,
     },
