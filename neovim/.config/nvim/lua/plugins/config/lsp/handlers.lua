75123109805c5573a13cb5291c53803391345e41
k14lb3 <k14lb3@gmail.com>
2023-04-07 16:38:23 +0800

    [nvim] 🐛 Fix deprecated `cmp-nvim-lsp` method (`update_capabilities`)

--- neovim/.config/nvim/lua/plugins/config/lsp/handlers.lua
+++ neovim/.config/nvim/lua/plugins/config/lsp/handlers.lua
@@ -112,6 +112,6 @@ M.on_attach = function(client, bufnr)
   end
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
